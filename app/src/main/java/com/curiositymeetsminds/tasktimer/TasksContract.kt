63029cb82de279aa583df93aa55e706aec0a5dc3
sounritesh <sounritesh@gmail.com>
2020-07-01 18:22:07 +0530

    TasksContract class added

--- app/src/main/java/com/curiositymeetsminds/tasktimer/TasksContract.kt
+++ app/src/main/java/com/curiositymeetsminds/tasktimer/TasksContract.kt
@@ -1,4 +1,19 @@
 package com.curiositymeetsminds.tasktimer
 
-class TasksContract {
+import android.provider.BaseColumns
+
+//This class is used to define the constant that will be needed to access the data of our Tasks table
+object TasksContract {
+
+//    const keyword is valid inside an object unlike classes
+    internal const val TABLE_NAME = "Tasks"
+
+//    an object inside an object is like a companion object but the companion keyword
+//    is not applicable inside an object
+    object columns {
+        const val ID = BaseColumns._ID // we can also simply use "_id", however this constant is provided by google through BaseColumns interface
+        const val TASK_NAME = "Name"
+        const val TASK_DESCRIPTION = "Description"
+        const val TASK_SORT_ORDER = "SortOrder"
+    }
 }
